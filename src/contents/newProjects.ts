47c63121433a06c5d422bb1dc41114fcd3ea8e9b
Natalia Navarro <nataliasoledadnavarro@gmail.com>
2025-08-02 22:41:49 -0300

    feat: Agrego card para listar nuevos proyectos. Componente Chip reutilizable.

---
+++ src/contents/newProjects.ts
@@ -0,0 +1,14 @@
+import {NewProjectProps} from "@/types";
+
+export const newProjects: NewProjectProps[] = [
+  {
+    id: 1,
+    title: "Template React 19 + Vite",
+    description:
+      "Template base para proyectos con React 19, TypeScript, Tailwind CSS y Vite. Ideal para empezar rápido con las nuevas features.",
+    image: "/img/template.svg",
+    tag: "Nuevo",
+    githubLink: "https://github.com/Nataliasoledadnavarro/template_react_19",
+    demoLink: "",
+  },
+];
