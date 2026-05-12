5c2b0d8f6a645e7af8f9a8e841caca8a813c6ad7
natalia navarro <natalia.navarro@eldars.com.ar>
2026-05-12 12:17:59 -0300

    fix: Actualiza skills

--- src/contents/skills.tsx
+++ src/contents/skills.tsx
@@ -1,21 +1,28 @@
 import { Skills } from "@/types/index";
 //Icons
-import { SiSonarqube, SiBulma, SiMui, SiIonic, SiJira } from "react-icons/si";
 import {
-  FaHtml5,
-  FaCss3Alt,
-  FaReact,
-  FaSass,
-  FaBootstrap,
-  FaAngular,
-  FaGitAlt,
-  FaGitlab,
-  FaGithub,
-  FaJenkins,
-} from "react-icons/fa";
+  SiSonarqube,
+  SiMui,
+  SiJira,
+  SiTypescript,
+  SiShadcnui,
+} from "react-icons/si";
+
+import { FaHtml5, FaReact, FaSass, FaGitAlt } from "react-icons/fa";
+
 import { IoLogoJavascript } from "react-icons/io5";
+
 import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
+
 import { DiScrum } from "react-icons/di";
+
+import { TbMapStar } from "react-icons/tb";
+
+import { MdAccessibility } from "react-icons/md";
+
+import { GiBrain } from "react-icons/gi";
+
+import { IoSparklesSharp } from "react-icons/io5";
 
 export const skills: Skills[] = [
   {
@@ -23,12 +30,12 @@ export const skills: Skills[] = [
     icon: <FaHtml5 className="h-7 w-7" />,
   },
   {
-    name: "CSS",
-    icon: <FaCss3Alt className="h-7 w-7" />,
-  },
-  {
     name: "JavaScript",
     icon: <IoLogoJavascript className="h-7 w-7" />,
+  },
+  {
+    name: "TypeScript",
+    icon: <SiTypescript className="h-7 w-7" />,
   },
   {
     name: "React",
@@ -39,52 +46,28 @@ export const skills: Skills[] = [
     icon: <RiNextjsFill className="h-7 w-7" />,
   },
   {
+    name: "Tailwind CSS",
+    icon: <RiTailwindCssFill className="h-7 w-7" />,
+  },
+  {
+    name: "Shadcn UI",
+    icon: <SiShadcnui className="h-7 w-7" />,
+  },
+  {
     name: "Sass",
     icon: <FaSass className="h-7 w-7" />,
-  },
-  {
-    name: "Bulma",
-    icon: <SiBulma className="h-7 w-7" />,
-  },
-  {
-    name: "Bootstrap",
-    icon: <FaBootstrap className="h-7 w-7" />,
   },
   {
     name: "Material UI",
     icon: <SiMui className="h-7 w-7" />,
   },
   {
-    name: "Tailwind CSS",
-    icon: <RiTailwindCssFill className="h-7 w-7" />,
-  },
-  {
-    name: "Ionic",
-    icon: <SiIonic className="h-7 w-7" />,
-  },
-  {
-    name: "Angular",
-    icon: <FaAngular className="h-7 w-7" />,
-  },
-  {
     name: "Git",
     icon: <FaGitAlt className="h-7 w-7" />,
   },
   {
-    name: "GitLab",
-    icon: <FaGitlab className="h-7 w-7" />,
-  },
-  {
-    name: "GitHub",
-    icon: <FaGithub className="h-7 w-7" />,
-  },
-  {
     name: "SonarQube",
     icon: <SiSonarqube className="h-7 w-7" />,
-  },
-  {
-    name: "Jenkins",
-    icon: <FaJenkins className="h-7 w-7" />,
   },
   {
     name: "Jira",
@@ -93,5 +76,21 @@ export const skills: Skills[] = [
   {
     name: "Scrum",
     icon: <DiScrum className="h-7 w-7" />,
+  },
+  {
+    name: "UX/UI",
+    icon: <TbMapStar className="h-7 w-7" />,
+  },
+  {
+    name: "Accesibilidad Web",
+    icon: <MdAccessibility className="h-7 w-7" />,
+  },
+  {
+    name: "IA Generativa",
+    icon: <GiBrain className="h-7 w-7" />,
+  },
+  {
+    name: "AI Workflows",
+    icon: <IoSparklesSharp className="h-7 w-7" />,
   },
 ];
